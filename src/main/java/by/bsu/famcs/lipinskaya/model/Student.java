68dda8b6ccc77b6f4adfb8540eaa66369e34e651
LipinskayaAlena <lipinskaya_alena@mail.ru>
2016-12-12 15:45:10 +0300

    createModelStudent

--- src/main/java/by/bsu/famcs/lipinskaya/model/Student.java
+++ src/main/java/by/bsu/famcs/lipinskaya/model/Student.java
@@ -1,7 +1,36 @@
 package by.bsu.famcs.lipinskaya.model;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
  * Created by Asus on 12.12.2016.
  */
-public class Student {
+@Entity
+@Table(name="student")
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id_student", nullable = false)
+    private long id_student;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    public long getId_student() { return this.id_student; }
+
+    public void setId_student(long id_student) { this.id_student = id_student; }
+
+    public String getName() { return this.name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return  this.email; }
+
+    public void setEmail(String email) { this.email = email; }
+
 }
